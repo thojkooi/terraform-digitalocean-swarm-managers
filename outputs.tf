318c92f293f64fb98bc4ac1179bdcc2b9d71289e
Thomas Kooi <thomasskooi@live.nl>
2018-01-21 14:12:44 +0100

    #5 fix terraform error during tf plan - Make use of lookup with a default value of empty string

--- outputs.tf
+++ outputs.tf
@@ -9,13 +9,13 @@ output "ipv4_addresses_private" {
 }
 
 output "manager_token" {
-  value       = "${data.external.swarm_tokens.result.manager}"
+  value       = "${lookup(data.external.swarm_tokens.result, "manager", "")}"
   description = "The Docker Swarm manager join token"
   sensitive   = true
 }
 
 output "worker_token" {
-  value       = "${data.external.swarm_tokens.result.worker}"
+  value       = "${lookup(data.external.swarm_tokens.result, "worker", "")}"
   description = "The Docker Swarm worker join token"
   sensitive   = true
 }
