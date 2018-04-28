a53951061d11c69537d096219a2e7102e5cd1aa1
Thomas Kooi <thomasjkooi@gmail.com>
2018-04-28 19:00:12 +0200

    Fix workers example

--- examples/add-workers/main.tf
+++ examples/add-workers/main.tf
@@ -13,14 +13,12 @@ module "swarm-cluster-managers" {
   source          = "../../"
   total_instances = 1
   region          = "ams3"
-  do_token        = "${var.do_token}"
   ssh_keys        = "${var.ssh_keys}"
   domain          = "do.example.com"
 }
 
 module "workers" {
-  source   = "github.com/thojkooi/terraform-digitalocean-swarm-workers"
-  do_token = "${var.do_token}"
+  source = "github.com/thojkooi/terraform-digitalocean-swarm-workers"
 
   size            = "s-1vcpu-1gb"
   name            = "web"
@@ -29,7 +27,6 @@ module "workers" {
   total_instances = 1
   ssh_keys        = "${var.ssh_keys}"
 
-  manager_public_ip  = "${element(module.swarm-cluster-managers.ipv4_addresses, 0)}"
   manager_private_ip = "${element(module.swarm-cluster-managers.ipv4_addresses_private, 0)}"
   join_token         = "${module.swarm-cluster-managers.worker_token}"
 }
